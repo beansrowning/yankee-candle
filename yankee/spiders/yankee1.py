57be2f689cf3867232a87cd1c3791c719e08a40b
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2023-08-21 21:21:54 +0000

    Updates from devcontainer

--- yankee/spiders/yankee1.py
+++ yankee/spiders/yankee1.py
@@ -1,30 +1,12 @@
 from scrapy import Spider, Request
 
-
-class YankeecandleSpider(Spider):
+class YankeecandleSpider1(Spider):
     user_agent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
 
-    name = ""
+    name = "yankeeCandle1"
     allowed_domains = ["www.amazon.com"]
-    start_urls = []
-    custom_feed = {}
-
-    def __init__(
-            self,
-            name = "yankeeCandle",
-            url="https://www.amazon.com/Yankee-Candle-Large-Balsam-Cedar/product-reviews/B000JDGC78/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews/",
-            out_file="data/%(name)s-amazon-candle.csv"):
-
-        self.name = name
-        self.start_urls.append(url)
-        self.custom_feed.update({out_file: "csv"})
-        self.custom_settings = {"FEED_URI": out_file, "FEED_FORMAT": "csv"}
-
-    @classmethod
-    def update_settings(cls, settings):
-        super().update_settings(settings)
-        settings.setdefault("FEEDS", {}).update(cls.custom_feed)
-
+    start_urls = ["https://www.amazon.com/Yankee-Candle-Large-Balsam-Cedar/product-reviews/B000JDGC78/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews/"]
+    
     def parse(self, response):
         for review in response.xpath("//div[@data-hook='review']"):
             yield {
